9e3dd2548360df5f392c8d5d1104c6229e0a58d6
am8256 <mehtaarnav02@gmail.com>
2021-09-23 20:01:08 +0530

    first commit

---
+++ CoreProject/AccountService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreProject
+{
+    class AccountService
+    {
+    }
+}
---
+++ CoreProject/EmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreProject
+{
+    class EmployeeService
+    {
+        public EmployeeService()
+        {
+
+        }
+    }
+}
